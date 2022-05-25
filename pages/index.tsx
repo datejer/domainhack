11afa81af144f0cf2da03f6d56928f32cbcc761d
ejer <ejer.priv@wp.pl>
2022-05-25 16:34:37 +0200

    :memo: Add info about API endpoint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 import { SEO } from "../components/common/SEO";
 import { Search } from "../components/search/Search";
 
@@ -13,6 +14,17 @@ const Home: NextPage = () => {
         example, "bir.ds", "examp.le" or "hello.world". Domain hacks offer the ability to produce
         shorter domain names. This makes them potentially valuable as redirectors, pastebins, base
         domains from which to delegate subdomains and URL shortening services.
+      </p>
+      <h2>i can has json?</h2>
+      <p>
+        <code>
+          curl https://domainhack.ejer.gq/api/domains?input=
+          {`\<yourquery\>`}
+        </code>
+        <br />
+        <Link href="/api/domains?input=ilikepizza">
+          <a target="_blank">Example API response</a>
+        </Link>
       </p>
     </>
   );
